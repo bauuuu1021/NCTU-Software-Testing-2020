c2a4a393223a6d070f3eca26f6997cdd71f32349
bauuuu1021 <bboy010554@icloud.com>
2020-03-23 21:58:26 +0800

    Upload lab2
    JUnit practice of testing class PriorityQueue
    - 5 Parameterized class testcases
    - 3 Exceptions thrown by PriorityQueue

---
+++ Lab2/PriorityQueue.java
@@ -0,0 +1,62 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.PriorityQueue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@RunWith(value=Parameterized.class)
+public class PriorityQueueTest {
+
+    private PriorityQueue<Integer> random, correct, testQueue;
+
+    @Parameters
+    public static Collection testCase() {
+        return Arrays.asList(new Integer[][][] {
+                {{2, 3, 1}, {1, 2, 3}},
+                {{1, 3, -1}, {-1, 1, 3}},
+                {{100, -1, 8, 32}, {-1, 8, 32, 100}},
+                {{5, 10, 30, 20, 15}, {5, 10, 15, 20, 30}},
+                {{-800, 5, 67, -44, 465, 7412}, {-800, -44, 5, 67, 465, 7412}}
+        });
+    }
+
+    public PriorityQueueTest(Integer[] a, Integer[] b) {
+        random = new PriorityQueue<Integer>();
+        correct = new PriorityQueue<Integer>();
+        for (int i = 0; i<a.length; i++) {
+            random.add(a[i]);
+            correct.add(b[i]);
+        }
+    }
+
+    @Test
+    public void pollTest() {
+        while (!correct.isEmpty()) {
+            assertEquals(correct.poll(), random.poll());
+        }
+    }
+
+    @Test
+    public void add_null() {
+        testQueue = new PriorityQueue<Integer>();
+        Exception exception = assertThrows(NullPointerException.class, ()->testQueue.add(null));
+    }
+
+    @Test
+    public void create_nullArr() {
+        testQueue = new PriorityQueue<Integer>();
+        Integer[] arr = null;
+        Exception exception = assertThrows(NullPointerException.class, ()->testQueue.toArray(arr));
+    }
+
+    @Test
+    public void wrong_type() {
+        Exception exception = assertThrows(IllegalArgumentException.class, ()->new PriorityQueue<Integer>(-1));
+    }
+}
